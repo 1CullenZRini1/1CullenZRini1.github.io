98e4337330eba2bfc885f8ea061fff5ef5e88aaf
1CullenZRini1 Rini <cullenrini@gmail.com>
2023-10-25 17:29:18 +0000

    platform

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -28,12 +28,12 @@ $(function () {
      * Comment the lines out to remove the grid
      */
 
-    // for (let i = 100; i < canvas.width; i += 100) {
-    //   createPlatform(i, canvas.height, -1, -canvas.height);
-    // }
-    // for (let i = 100; i < canvas.height; i += 100) {
-    //   createPlatform(canvas.width, i, -canvas.width, -1);
-    // }
+    for (let i = 100; i < canvas.width; i += 100) {
+     createPlatform(i, canvas.height, -1, -canvas.height);
+     }
+     for (let i = 100; i < canvas.height; i += 100) {
+       createPlatform(canvas.width, i, -canvas.width, -1);
+     }
 
     /////////////////////////////////////////////////
     //////////ONLY CHANGE BELOW THIS POINT///////////
@@ -43,27 +43,31 @@ $(function () {
     // Create platforms
     // You must decide the x position, y position, width, and height of the platforms
     // example usage: createPlatform(x,y,width,height)
-
-
-
-    
+    createPlatform(300, 590, 200, 20)
+    createPlatform(600, 480, 100, 20)
+    createPlatform(300, 440, 200, 10)
+    createPlatform(700, 690, 300, 20)
+    createPlatform(800, 370, 350, 15)
+    createPlatform(500, 200, 532, 10)
+    createPlatform(1200, 300, 430, 15)
     // TODO 2
     // Create collectables
     // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
     // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
     // example usage: createCollectable(type, x, y, gravity, bounce)
-
-
-
+    createCollectable("steve", 600, 400, 6, 0.7)
+    createCollectable("diamond", 950, 280, 9, .6 )
+    createCollectable( "database", 380, 480, 7, .4)
+    createCollectable( "database", 840, 550, 7, .3)
 
     // TODO 3
     // Create cannons
     // You must decide the wall you want the cannon on, the position on the wall, and the time between shots in milliseconds
     // Your wall choices are: 'top' 'left' 'right' and 'bottom'
     // example usage: createCannon(side, position, delay, width, height)
-
-
-
+    createCannon("right", 500, 5000)
+    createCannon("top", 500, 3000)
+    createCannon("top", 860, 3000)
 
     /////////////////////////////////////////////////
     //////////ONLY CHANGE ABOVE THIS POINT///////////
